b2596fd79a506642277037b7986fc8290930079f
Daebeom Cho <dcho@c4r10s7.42seoul.kr>
2022-01-03 20:44:06 +0900

    C++ Module Clear!

--- cpp08/ex02/src/mutantstack.hpp
+++ cpp08/ex02/src/mutantstack.hpp
@@ -9,48 +9,104 @@ template <typename T>
 class Mutantstack : public std::stack<T>
 {
 	public:
-	class iterator
-	{
-		public:
-			iterator() : ptr(NULL) {}
-			iterator(T* param) : ptr(param) {}
-			~iterator() { ptr = NULL; }
-			iterator(const iterator &copy) { *this = copy; }
-			T& getPtr() const { return (ptr); }
-			iterator& operator = (const iterator& i) { ptr = i.ptr; return (*this); }
-			iterator& operator ++() { ++ptr; return *this; }
-			iterator operator ++(int) { iterator temp(ptr); ++(ptr); return temp; }
-			iterator& operator --() { --ptr; return *this; }
-			iterator operator --(int) { iterator temp(ptr); --(ptr); return temp; }
-		private:
-			T *ptr;
-	};
+		class iterator
+		{
+			public:
+				iterator() : ptr(NULL) {}
+				iterator(T *param) : ptr(param) {}
+				~iterator() { ptr = NULL; }
+				iterator(const iterator &copy) { *this = copy; }
+				iterator &operator=(const iterator &param)
+				{
+					ptr = param.ptr;
+					return (*this);
+				}
+				iterator operator + (const int &i)
+				{
+					iterator temp(ptr + i);
+					return (temp);
+				}
+				iterator operator - (const int &i)
+				{
+					iterator temp(ptr - i);
+					return (temp);
+				}
+				iterator &operator++()
+				{
+					++ptr;
+					return *this;
+				}
+				iterator operator++(int)
+				{
+					iterator temp(ptr);
+					++(ptr);
+					return temp;
+				}
+				iterator &operator--(void)
+				{
+					--ptr;
+					return *this;
+				}
+				iterator operator--(int)
+				{
+					iterator temp(ptr);
+					--(ptr);
+					return temp;
+				}
+				bool operator > (const iterator &param)
+				{
+					return (this->ptr > param.ptr);
+				}
+				bool operator < (const iterator &param)
+				{
+					return (this->ptr < param.ptr);
+				}
+				bool operator >= (const iterator &param)
+				{
+					return (this->ptr >= param.ptr);
+				}
+				bool operator <= (const iterator &param)
+				{
+					return (this->ptr <= param.ptr);
+				}
+				bool operator == (const iterator &param)
+				{
+					return (this->ptr == param.ptr);
+				}
+				bool operator != (const iterator &param)
+				{
+					return (this->ptr != param.ptr);
+				}
+				T &operator * (void)
+				{
+					return (*ptr);
+				}
+			private:
+				T *ptr;
+		};
 	Mutantstack() {}
 	~Mutantstack() {}
 	Mutantstack(Mutantstack &copy) { *this = copy; }
 	Mutantstack &operator=(const Mutantstack &ms)
 	{
-		*this = ms;
-		return (ms);
+		if (this == &ms)
+			return (*this);
+		std::stack<T>::operator=(ms);
+		return (*this);
 	}
-	iterator& begin()
+	iterator begin()
 	{
-		//iterator i( &(this->top()) - (this->size()) );
-		iterator i = new iterator(&(this->top()) - (this->size()));
-		return i;
+		iterator res((&this->top()) - (this->size() - 1));
+		return res;
 	}
-	iterator& end()
+	iterator end()
 	{
-		iterator i( &(this->top()) + 1 );
-		return i;
+		iterator res((&this->top()) + 1);
+		return res;
 	}
 };
 
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const T& i)
-{
-	os << i.ptr;
-    return os;
-}
-
 #endif
+
+
+
